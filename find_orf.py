081ee50da7678523a8c1780b529d6924a8e3294a
Kayci Messerly <kim0009@auburn.edu>
2023-03-05 18:22:48 -0600

    Hard coded standard start and stop codons.

--- find_orf.py
+++ find_orf.py
@@ -208,7 +208,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    orf_pattern_str = r'(AUG)([ACGU]{3})*(UAA|UAG|UGA)'
     ##########################################################################
 
     # Create the regular expression object
